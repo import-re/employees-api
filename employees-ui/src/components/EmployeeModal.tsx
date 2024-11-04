80e29e15e871bd9921a06661bf7e77248348991c
Eva Kazakovskaia <eva.kazakovskaia@pipedrive.com>
2024-11-04 16:56:52 +0200

    added frontend

--- employees-ui/src/components/EmployeeModal.tsx
+++ employees-ui/src/components/EmployeeModal.tsx
@@ -9,11 +9,18 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ employee, onClose }) => {
     if (!employee) return null;
 
+
     return (
         <div className="modal-overlay">
             <div className="modal-content">
                 <h2>{employee.name}</h2>
                 <p>Title: {employee.title}</p>
+                <p>Tribe: {employee.tribe.tribe_name}</p>
+                <img 
+                    src="https://controlio.net/i/svg/brand-figure.svg" 
+                    alt={`${employee.name}'s profile`} 
+                    className="employee-image" 
+                />
                 <button>Edit</button>
                 <button onClick={onClose}>Close</button>
             </div>
